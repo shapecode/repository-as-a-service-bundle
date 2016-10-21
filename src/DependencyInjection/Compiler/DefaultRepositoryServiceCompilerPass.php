ce6200329615628ee3901ec7f5606879d16eba57
Nikita Loges <git@nikita-loges.de>
2016-10-21 16:48:29 +0200

    add underscore support

--- src/DependencyInjection/Compiler/DefaultRepositoryServiceCompilerPass.php
+++ src/DependencyInjection/Compiler/DefaultRepositoryServiceCompilerPass.php
@@ -2,8 +2,8 @@
 
 namespace Shapecode\Bundle\RasSBundle\DependencyInjection\Compiler;
 
-use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Mapping\ClassMetadata;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
@@ -11,8 +11,9 @@ use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * Class DefaultRepositoryServiceCompilerPass
+ *
  * @package Shapecode\Bundle\RasSBundle\DependencyInjection\Compiler
- * @author Nikita Loges
+ * @author  Nikita Loges
  * @company tenolo GbR
  */
 class DefaultRepositoryServiceCompilerPass implements CompilerPassInterface
@@ -44,6 +45,7 @@ class DefaultRepositoryServiceCompilerPass implements CompilerPassInterface
 
             $name = $this->generateServiceName($reflectionClass);
             $aliasName = $this->generateAliasName($reflectionClass);
+            $aliasNameUnderscore = $this->generateAliasNameUnderscore($reflectionClass);
             $repositoryName = EntityRepository::class;
 
             if ($m->customRepositoryClassName) {
@@ -63,11 +65,16 @@ class DefaultRepositoryServiceCompilerPass implements CompilerPassInterface
             if (!$container->hasAlias($aliasName)) {
                 $container->setAlias($aliasName, $name);
             }
+
+            if (!$container->hasAlias($aliasNameUnderscore)) {
+                $container->setAlias($aliasNameUnderscore, $name);
+            }
         }
     }
 
     /**
      * @param \ReflectionClass $class
+     *
      * @return string
      */
     protected function generateServiceName(\ReflectionClass $class)
@@ -77,10 +84,27 @@ class DefaultRepositoryServiceCompilerPass implements CompilerPassInterface
 
     /**
      * @param \ReflectionClass $class
+     *
      * @return string
      */
     protected function generateAliasName(\ReflectionClass $class)
     {
         return strtolower($class->getShortName()) . '_repository';
+    }
+
+    /**
+     * @param \ReflectionClass $class
+     *
+     * @return string
+     */
+    protected function generateAliasNameUnderscore(\ReflectionClass $class)
+    {
+        $name = $class->getShortName();
+        $name = preg_split('/(?=[A-Z])/', $name);
+        $name = array_filter($name, function ($el) {
+            return !empty($el);
+        });
+
+        return strtolower(implode('_', $name)) . '_repository';
     }
 }
