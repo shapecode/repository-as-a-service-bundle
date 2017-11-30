5cfe6e057bdeb5597ff9b02a9fb6a0ad085cfd6e
Nikita Loges <nl@tenolo.de>
2017-11-30 17:11:12 +0100

    doctrine bundle 1.8 compability

--- src/DependencyInjection/Compiler/RepositoryFactoryCompilerPass.php
+++ src/DependencyInjection/Compiler/RepositoryFactoryCompilerPass.php
@@ -4,6 +4,7 @@ namespace Shapecode\Bundle\RasSBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * Class RepositoryFactoryCompilerPass
@@ -13,16 +14,36 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
  */
 class RepositoryFactoryCompilerPass implements CompilerPassInterface
 {
+
+    const DOCTRINE_FACTORY_SERVICE_NAME = 'doctrine.orm.container_repository_factory';
+    const SC_FACTORY_SERVICE_NAME = 'shapecode_raas.doctrine.repository_factory';
+    const SC_FACTORY_SERVICE_INNER_NAME = 'shapecode_raas.doctrine.repository_factory.inner';
+
     /**
      * @inheritDoc
      */
     public function process(ContainerBuilder $container)
     {
         // custom factory
-        $factory = $container->findDefinition('shapecode_raas.doctrine.repository_factory');
+        $factory = $container->findDefinition(self::SC_FACTORY_SERVICE_NAME);
 
-        // replace default repository factory
-        $container->findDefinition('doctrine.orm.configuration')->addMethodCall('setRepositoryFactory', [$factory]);
+        /*
+         * this feature is since doctrine bundle 1.8 default
+         * we have to replace it
+         * @since 1.8
+         */
+        if ($container->has(self::DOCTRINE_FACTORY_SERVICE_NAME)) {
+
+            $factory->setDecoratedService(self::DOCTRINE_FACTORY_SERVICE_NAME);
+            $factory->replaceArgument(1, new Reference(self::SC_FACTORY_SERVICE_INNER_NAME));
+
+        } // @before 1.8
+        else {
+
+            // replace default repository factory
+            $container->findDefinition('doctrine.orm.configuration')->addMethodCall('setRepositoryFactory', [$factory]);
+        }
+
     }
 
 }
